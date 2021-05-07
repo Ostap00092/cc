c30901b1731bb4041a28f897606753ef38ce5d1e
UnderOneBreath <63227827+UnderOneBreath@users.noreply.github.com>
2021-05-08 00:15:46 +0900

    version 1.2

--- index.php
+++ index.php
@@ -4,26 +4,30 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="css/all.css">
 	<link rel="stylesheet" href="css/index.css">
 	<title>Creation Community</title>
 </head>
 <body>
-	<?php
-	if ($_COOKIE['onauto'] != 1) {
-		echo "<h2>Приветствуем, <br> мы рады видеть вас.
-		</h2>
-		<a href='go.php'><div class='btn'>
-			Начать
-		</div></a>";
-	}?>
+	<div class="justcenter">
+		<?php
+		if ($_COOKIE['onauto'] != 1) {
+			echo "<h2>Приветствуем, <br> мы рады видеть вас.
+			</h2>
+			<a href='go.php'><div class='btn'>
+				Начать
+			</div></a>";
+		}?>
 
-	<?php echo "<a href='watch.php'><div class='btn'>Смотреть публикации</div></a>";
-	?>
-	
-	<?php
-	if ($_COOKIE['onauto'] == 1) {
+		<?php echo "<a href='watch.php'><div class='btn'>Смотреть публикации</div></a>";
+		?>
 		
-		require 'php/menu.php';
-	}?>
+		<?php
+		if ($_COOKIE['onauto'] == 1) {
+			
+			require 'php/menu.php';
+		}?>
+	</div>
+
 </body>
 </html>
